e9f6bcefeb5412135cf54fab67b5e518b9d91ef6
ADIASFOTIO <jnrfotio@gmail.com>
2023-07-11 15:02:22 +0200

    added the new method

--- src/main/java/com/adias/gestionestock/GestionestockApplication.java
+++ src/main/java/com/adias/gestionestock/GestionestockApplication.java
@@ -8,6 +8,10 @@ public class GestionestockApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionestockApplication.class, args);
+
+	}
+	public static void printttt(){
+		System.out.println("ci sono io");
 	}
 
 }
